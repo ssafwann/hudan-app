afa8111151073bf9e199c387c0b59cc1b5637802
ssafwann <safwan917@gmail.com>
2025-05-20 16:08:49 +0800

    add hadith json file

--- Hudan/ContentView.swift
+++ Hudan/ContentView.swift
@@ -1,21 +1,10 @@
-//
-//  ContentView.swift
-//  Hudan
-//
-//  Created by Malik Safwan on 20/5/2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Daily Hadith!")
         }
-        .padding()
     }
 }
 
