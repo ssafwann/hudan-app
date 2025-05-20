1f8543bc29deca7e422caf1f460deba45f144a5b
ssafwann <safwan917@gmail.com>
2025-05-20 16:04:35 +0800

    widget config

---
+++ HudanWidget/HudanWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HudanWidgetBundle.swift
+//  HudanWidget
+//
+//  Created by Malik Safwan on 20/5/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HudanWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HudanWidget()
+    }
+}
