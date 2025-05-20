f1ea2504a53cee62c601f413c44cd840de1ca5f2
Malik Safwan <safwan917@gmail.com>
2025-05-20 15:50:36 +0800

    Initial Commit

---
+++ Hudan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hudan
+//
+//  Created by Malik Safwan on 20/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hudan/HudanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HudanApp.swift
+//  Hudan
+//
+//  Created by Malik Safwan on 20/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct HudanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
