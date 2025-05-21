7fd98fa80a47e3b08a61b9c0d535d6233289c979
ssafwann <safwan917@gmail.com>
2025-05-21 11:24:18 +0800

    create hadith model

--- Hudan/Models/Hadith.swift
+++ Hudan/Models/Hadith.swift
@@ -1,1 +1,14 @@
 // hadith data model
+
+import Foundation
+
+struct Hadith: Identifiable, Codable {
+    let id: Int
+    let narrator: String
+    let arabic: String
+    let english: String
+    let summary: String
+    let ref: String
+    let inbook_ref: String
+    let grade: String
+}
